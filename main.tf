96343177ef497c999d50657fcecfef71b320aa24
jinseok9338 <jinseok9338@gmail.com>
2021-03-05 16:17:23 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIAJAIEHOKODFBWFPAQ"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
 
 
